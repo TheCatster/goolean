d4b39a95d8fb7628e468d69d959924943010e203
Daniil Rose <daniil.rose@posteo.org>
2023-09-26 14:42:16 -0400

    Add tokenize function

--- pkg/goolean/goolean.go
+++ pkg/goolean/goolean.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func BeginShell() (result string) {
@@ -27,5 +28,34 @@ func BeginShell() (result string) {
 }
 
 func handleInput(input string) (result string) {
-	return input
+	/*
+	 * Sample input: (NOT A AND NOT B) OR (NOT A OR NOT B)
+	 * Expected output: NOT A OR NOT B
+	 * Truth Table:
+	 */
+	data := strings.Split(input, " ")
+	tokens := tokenize(data)
+
+	return strings.Join(tokens, ", ")
+}
+
+func tokenize(data []string) []string {
+	var tokens []string
+
+	for i := range data {
+		p := data[i]
+		if strings.Contains(p, "(") {
+			cnt := strings.Join(strings.Split(p, "("), "")
+			tokens = append(tokens, "(")
+			tokens = append(tokens, cnt)
+		} else if strings.Contains(p, ")") {
+			cnt := strings.Join(strings.Split(p, ")"), "")
+			tokens = append(tokens, cnt)
+			tokens = append(tokens, ")")
+		} else {
+			tokens = append(tokens, p)
+		}
+	}
+
+	return tokens
 }
